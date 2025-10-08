cfeadd021dc1bc0c262c8dbe0a8ba5b1ce76a57d
Kate Suraev <ksuraev@outlook.com>
2025-10-08 16:06:35 +1100

    Add execution script to run CMake project with varying iteration counts

---
+++ executionscript.sh
@@ -0,0 +1,13 @@
+# Run Cmake project for various iteration counts
+#!/bin/bash
+ITERATIONS=(100 500 1000)
+
+# Build the project
+cmake -S . -B build
+cmake --build build -config Release
+
+for i in "${ITERATIONS[@]}"
+do
+   echo "Running with $i iterations"
+   ./build/Project $i
+done
