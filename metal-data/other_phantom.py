81f8654ad2e7387b48902850cb97e1945bfced27
Kate Suraev <ksuraev@outlook.com>
2025-10-14 21:46:39 +1100

    Refactor phantom model number declaration and update output filename format

--- metal-data/other_phantom.py
+++ metal-data/other_phantom.py
@@ -8,11 +8,12 @@ import matplotlib.pyplot as plt
 libpath = os.path.join(os.path.dirname(tomophantom.__file__), "phantomlib", "Phantom2DLibrary.dat")
 
 # Generate a 2D phantom - specify the model number and the size
-phantom = TomoP2D.Model(13, 256, libpath)
+model_number = 13
+phantom = TomoP2D.Model(model_number, 256, libpath)
 
 # Normalise the phantom to [0, 1]
 phantom = (phantom - np.min(phantom)) / (np.max(phantom) - np.min(phantom))
 
 # Save the phantom to a txt file
-output_filename = os.path.join(os.path.dirname(__file__), "phantom_model13.txt")
+output_filename = os.path.join(os.path.dirname(__file__), f"phantom_model_f{model_number}.txt")
 np.savetxt(output_filename, phantom, fmt="%.8f")
